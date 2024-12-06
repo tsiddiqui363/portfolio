d6a5efe0ec31c88a954f1c2391d1e6fc20cba768
tsiddiqui363 <tsiddiqui@mun.ca>
2024-12-06 00:05:18 -0330

    Updated bio section in HTML for website

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -8,20 +8,18 @@ const About = () => {
       <div className={styles.contentBox}>
         <h1>About Me</h1>
         <p>
-          Ever since I can remember, technology has captivated me. As a child, games like Legend of Zelda and Fire Emblem sparked a dream within me—a dream to create immersive, unforgettable experiences just like them. As I grew older, my interests evolved, and I found myself drawn to the world of RPGs and MOBA games. It was during this time that I discovered Unity and Unreal Engine, realizing that these tools could be the gateway to fulfilling my dream of becoming a game developer.
+          Technology has always been at the core of my ambitions, driving my passion for creating innovative, data-driven solutions. My programming journey began in high school, where I gained a strong foundation in C programming and Python. Since then, I have cultivated my expertise in machine learning, data analysis, and statistical modeling, focusing on using these tools to solve complex problems. Beyond programming, my passion for storytelling and creative problem-solving naturally extends to front-end development, where I combine visuals and interactivity to craft engaging user experiences.
         </p>
         <p>
-          My journey into the world of programming began in high school, where I immersed myself in learning C programming and Python. These early experiences laid a solid foundation for my future endeavors and ignited a passion within me that continues to burn brightly.
+          Throughout my academic career, I have developed proficiency in Python, JavaScript, React, and machine learning libraries such as Pandas, scikit-learn, and Matplotlib. These experiences have enhanced my skills in data visualization, hypothesis testing, and algorithm development.
         </p>
         <p>
-          Beyond programming, I've always had a diverse range of interests. From sketching to digital painting, I've always been fascinated by visuals and their ability to evoke emotions and tell stories. This fascination naturally led me to focus on front-end development, where I found a deep sense of fulfillment and engagement.
+          My professional journey has also allowed me to develop excellent organizational and analytical abilities. As a Cash Room Processor at GardaWorld, I managed sensitive financial data with precision, reinforcing my attention to detail and commitment to accuracy. These skills, combined with my technical expertise, have prepared me to excel in roles that require blending data analysis, programming, and problem-solving.
         </p>
         <p>
-          While my academic achievements may not be stellar, my passion for coding is unwavering. When I'm engrossed in coding, time seems to stand still as I become fully immersed in perfecting every aspect of my program. I may not have the highest grades or the most prestigious qualifications, but what I lack in formal credentials, I make up for with dedication, creativity, and a relentless drive to learn and improve. Currently, my focus lies in web development and data analysis.
-        </p>
-        <p>
-          I've honed my skills in Python, C, JavaScript, HTML, CSS, and React, and I'm continuously seeking to expand my knowledge, with ongoing learning in C++ and Unreal Engine. Besides, I am working on machine learning tools such as pandas, scikit-learn, and matplotlib etc. My journey is one of passion, determination, and a deep-seated love for the world of technology—a world that continues to inspire and challenge me every day.
-        </p>
+          Currently, I am expanding my knowledge in AWS to enhance my understanding of scalable machine learning applications. I am passionate about leveraging data and AI to drive innovation, tackle real-world challenges, and contribute to meaningful projects in the fields of technology and data science.
+          </p>
+
         <Link to="home" smooth={true} duration={500}>Back to Home</Link>
       </div>
     </div>
